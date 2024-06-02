5c4a07f9c70530f47ceb33e293267bbc0d85574a
condoor1 <157987961+condoor1@users.noreply.github.com>
2024-06-02 12:41:43 -0400

    Update setup.sh
    sources .zshrc at the end of the script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 sudo apt install zsh fzf
 chsh -s /bin/zsh
+zsh
 
 ln -s "$HOME/github/dotfiles/.zshrc" "$HOME/.zshrc"
 ln -s "$HOME/github/dotfiles/.p10k.zsh" "$HOME/.p10k.zsh"
+
+source "$HOME/.zshrc"
