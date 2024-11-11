2526f5329de1f253f4e422b8fccd58f1c4067ee1
condoor1 <157987961+condoor1@users.noreply.github.com>
2024-11-10 23:48:40 -0500

    Update links.sh

--- links.sh
+++ links.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
-ln -s "$HOME/dotfiles/.p10k.zsh" "$HOME/.p10k.zsh"
+ln -s "$HOME/dotfiles/.config/zsh/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/dotfiles/.config/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
