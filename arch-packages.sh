b7be60d6fe76509fba68657434a1accd884f540f
condoor1 <157987961+condoor1@users.noreply.github.com>
2024-11-11 00:00:17 -0500

    Create arch-packages.sh

---
+++ arch-packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pacman -S --needed kitty firefox waybar wireplumber thunar wofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl sddm hyprlock yazi yay bluez bluez-util
+systemctl enable bluetoothctl
