8236baa9d47f2c15b1690467791eb95a5df0db16
condoor1 <157987961+condoor1@users.noreply.github.com>
2024-11-11 22:03:24 -0500

    Add files via upload

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pacman -S --needed kitty firefox waybar wireplumber thunar wofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl sddm hyprlock yazi yay bluez bluez-util
-systemctl enable bluetoothctl
+pacman -S --needed kitty firefox waybar wireplumber thunar rofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl sddm hyprlock yazi bluez bluez-utils fzf
+#systemctl enable bluetooth.service
+#yay -S wlogout
