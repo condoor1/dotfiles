8a1be03b80aafa9de21664468e283253cf492287
condoor1 <157987961+condoor1@users.noreply.github.com>
2024-11-11 22:09:41 -0500

    Update arch-packages.sh

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-pacman -S --needed kitty firefox waybar wireplumber thunar rofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl sddm hyprlock yazi bluez bluez-utils fzf
+pacman -S gnome kitty firefox waybar wireplumber thunar rofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl hyprlock yazi bluez bluez-utils fzf
+
 #systemctl enable bluetooth.service
 #yay -S wlogout
