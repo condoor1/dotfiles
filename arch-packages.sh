24b93a8b02758ba27d06719ffa53c2199ff2cd1c
condoor1 <larkin678@gmail.com>
2024-11-20 21:34:34 -0500

    config updates

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pacman -S gnome hyprland kitty firefox waybar wireplumber thunar rofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl hyprlock yazi bluez bluez-utils fzf neovim python-pywal base-devel
+sudo pacman -S gnome hyprland kitty firefox waybar wireplumber thunar rofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl hyprlock yazi bluez bluez-utils fzf neovim python-pywal base-devel neofetch spotify-launcher bitwarden tree man-db ffmpeg
 
-systemctl enable bluetooth.service gdm.service
+sudo systemctl enable bluetooth.service gdm.service
 
 cd $HOME
 git clone https://aur.archlinux.org/yay.git
@@ -12,3 +12,5 @@ makepkg -si
 
 cd $HOME
 yay -S wlogout
+
+sudo chsh -s /usr/bin/zsh
--- links.sh
+++ links.sh
@@ -2,3 +2,32 @@
 
 ln -s "$HOME/dotfiles/.config/zsh/.zshrc" "$HOME/.zshrc"
 ln -s "$HOME/dotfiles/.config/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
+
+mkdir $HOME/.config/waybar
+ln -s "$HOME/dotfiles/.config/waybar/config.jsonc" "$HOME/.config/waybar/config.jsonc"
+ln -s "$HOME/dotfiles/.config/waybar/style.css" "$HOME/.config/waybar/style.css"
+ln -s "$HOME/dotfiles/.config/waybar/theme.css" "$HOME/.config/waybar/theme.css"
+
+mkdir -p $HOME/.config/hypr/hyprland
+ln -s "$HOME/dotfiles/.config/hypr/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
+ln -s "$HOME/dotfiles/.config/hypr/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
+ln -s "$HOME/dotfiles/.config/hypr/hyprlock.conf" "$HOME/.config/hypr/hyprlock.conf"
+
+mkdir $HOME/.config/rofi
+ln -s "$HOME/dotfiles/.config/rofi/config.rasi" "$HOME/.config/rofi/config.rasi"
+
+### I'm copying this cause I'm to lazy to link each file. Maybe switch to gnu-stow? ##
+mkdir $HOME/.config/wallpaper
+cp -r $HOME/dotfiles/.config/wallpaper/* $HOME/.config/wallpaper/
+
+mkdir $HOME/.config/wlogout
+ln -s "$HOME/dotfiles/.config/wlogout/layout" "$HOME/.config/wlogout/layout"
+ln -s "$HOME/dotfiles/.config/wlogout/style.css" "$HOME/.config/wlogout/style.css"
+
+mkdir $HOME/.config/yazi
+ln -s "$HOME/dotfiles/.config/yazi/yazi.toml" "$HOME/.config/yazi/yazi.toml"
+ln -s "$HOME/dotfiles/.config/yazi/config.toml" "$HOME/.config/yazi/config.toml"
+
+## Again, too lazy to link everything ##
+mkdir $HOME/.scripts/
+cp -r $HOME/dotfiles/.scripts/* $HOME/.scripts/
