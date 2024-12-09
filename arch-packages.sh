de33b93ca9f137a188533c9bf13e1ef4dd5bf2fe
condoor1 <larkin678@gmail.com>
2024-12-09 10:12:28 -0500

    update

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S gnome hyprland kitty firefox waybar wireplumber thunar rofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl hyprlock yazi bluez bluez-utils fzf neovim python-pywal base-devel neofetch spotify-launcher bitwarden tree man-db ffmpeg neofetch hyprlock
+sudo pacman -S gnome hyprland kitty firefox waybar wireplumber thunar rofi hyprpaper cliphist zsh noto-fonts-emoji ttf-jetbrains-mono-nerd brightnessctl hyprlock yazi bluez bluez-utils fzf neovim python-pywal base-devel neofetch spotify-launcher bitwarden tree man-db ffmpeg neofetch hyprlock wget
 
 sudo systemctl enable bluetooth.service gdm.service
 
@@ -11,6 +11,6 @@ cd yay
 makepkg -si
 
 cd $HOME
-yay -S wlogout
+yay -S wlogout google-chrome
 
 sudo chsh -s /usr/bin/zsh
